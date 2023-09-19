8a162988102bf488c198305478effbbf43e89ada
Roger Marsh <roger.marsh@solentware.co.uk>
2023-09-19 21:01:35 +0000

    Add sort for DPT multi-step deferred update 'tape' files.
    See DPTDocs/dbaguide.html Appendix 1 from DPT_V3R0_DOCS.ZIP for formats.
    
    Existing content of tests directory moved to tests/runs to leave tests
    clear for sort module unittests.
    
    Multi-step deferred update is obsolete.  Single-step deferred update
    should be used instead except on x64 where it does not work and
    multi-step is the only option.

---
+++ package-template/tests/runs/create_x86_200000_5021_deferred.py
@@ -0,0 +1,25 @@
+# create_x86_200000_5021_deferred.py
+# Copyright 2023 Roger Marsh
+# License: BSD license
+
+"""Populate database defined in file_definitions module.
+
+Just want one to reorganize in x64 environment.
+
+DPT files cannot be created and initialized in the same run as the file is
+populated by deferred updates.
+
+(Possibily not quite true because the DPT audit trail suggests the first
+ file processed in such a run is populated before the job terminates
+ without giving any failure indication.)
+
+"""
+import run_test_create_databases
+
+
+if __name__ == "__main__":
+    print("start create_x86_200000_5021_deferred")
+    run_test_create_databases.rtcd_data_data_ord_inv(
+        default_records=200000, modulus=13
+    )
+    print("finish create_x86_200000_5021_deferred")
---
+++ package-template/tests/runs/reorganize_x64_200000_5021_deferred.py
@@ -0,0 +1,28 @@
+# reorganize_x64_200000_5021_deferred.py
+# Copyright 2023 Roger Marsh
+# License: BSD license
+
+"""Reorganize database defined in file_definitions module.
+
+This is known to work in x86 environment, although best to verify the
+run can work: the test is what about x64.
+
+"""
+import open_dpt_database
+import file_definitions
+
+
+if __name__ == "__main__":
+    database = open_dpt_database.open_dpt_database(
+        file_definitions.DATA_DATA_ORD_INV,
+        file_definitions.data_data_ord_inv(),
+    )
+    print("start reorganize_x64_200000_5021_deferred")
+    try:
+        for file, context in database.contexts.items():
+            print("enter reorganize for", file)
+            context.Reorganize()
+            print("leave reorganize for", file)
+        print("finish reorganize_x64_200000_5021_deferred")
+    finally:
+        database.close_database()
